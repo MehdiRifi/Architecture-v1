b464dfa1f07547984e9806b915f57e224eaf7515
Mehdi Rifi <mehdirifi51@gmail.com>
2019-11-21 12:06:43 +0100

    Add project files.

---
+++ CarsManager/Modules/RepositoryModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace Cars.Modules
+{
+    public class RepositoryModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+           // builder.RegisterType<CarRepository>().As<ICarRepository>().InstancePerLifetimeScope();
+        }
+    }
+}
---
+++ CarsManager/Startup.cs
@@ -0,0 +1,67 @@
+
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Cars.Modules;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace MediatR
+{
+    public class Startup
+    {
+        public Startup(IWebHostEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+         .SetBasePath(env.ContentRootPath)
+         .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+         .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+         .AddEnvironmentVariables();
+            this.Configuration = builder.Build();
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+            services.AddMvcCore()
+                .AddApiExplorer();
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+        }
+        public IConfigurationRoot Configuration { get; private set; }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new RepositoryModule());
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //use swagger
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+
+        }
+    }
+}
